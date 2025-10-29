95ac570dca76a0eaa7ca4831ea81e1d50ec8af62
Douglas Jasper <doug.jasper@trios.com>
2025-10-29 10:16:20 -0400

    Initial Commit

---
+++ DatePickerDemo/DatePickerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePickerDemoApp.swift
+//  DatePickerDemo
+//
+//  Created by Douglas Jasper on 2025-10-29.
+//
+
+import SwiftUI
+
+@main
+struct DatePickerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DatePickerDemoTests/DatePickerDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  DatePickerDemoTests.swift
+//  DatePickerDemoTests
+//
+//  Created by Douglas Jasper on 2025-10-29.
+//
+
+import Testing
+@testable import DatePickerDemo
+
+struct DatePickerDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
